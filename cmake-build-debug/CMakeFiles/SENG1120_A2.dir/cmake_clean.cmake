cb6de7427e0ce91d43c9d6eeb2991cf85f6958c1
Toby <tobydines@outlook.com>
2021-09-27 20:55:55 +1000

    Project creation

---
+++ cmake-build-debug/CMakeFiles/SENG1120_A2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SENG1120_A2.dir/main.cpp.o"
+  "SENG1120_A2.exe"
+  "SENG1120_A2.pdb"
+  "libSENG1120_A2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SENG1120_A2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SENG1120_A2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SENG1120_A2.
+# This may be replaced when dependencies are built.
